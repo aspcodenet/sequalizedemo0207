2d82ca9beb99c08cbdbf59e1ced62731d9e40048
aspcodenet <stefan.holmberg@systementor.se>
2024-02-07 10:09:55 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const readline = require('readline/promises');
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
+const { sequelize, Employee } = require('./models')
+const migrationhelper = require('./migrationhelper')
+
+
+
+
+async function listAll(){
+    for(const emp of employees){
+        console.log("************************")
+        console.log("ID:", emp.id)
+        console.log("Name:", emp.name)
+    }
+
+}
+
+async function createNew(){
+    console.log("** NEW ** ")
+    const name = await rl.question('Name:')
+    const salary = await rl.question('Hourly salary::')
+
+    // 
+    // let sql = "insert into Employee(name,salary,birthDate) values('"
+    // sql = sql + name + "',"
+    employees.push({
+        name:name,
+        birthDate:'1972-08-03',hourlySalary:   parseInt(salary)
+    })
+}
+
+
+
+async function main(){
+    await migrationhelper.migrate()
+    while(true){    
+        console.log('1. Lista alla employees')
+        console.log('2. Skapa employee')
+        console.log('3. Uppdatera employee')
+        console.log('4. Ta bort employee')
+        console.log('9. Avsluta')
+
+        const sel = await rl.question('Val:');  
+        if(sel == '1'){
+            await listAll()
+        }
+        if(sel == '2'){
+            await createNew()
+        }
+        if(sel == '3'){
+            updateOne()
+        }
+        if(sel == '4'){
+            deleteOne()
+        }
+        if(sel == '9'){
+            break
+        }
+    
+    }
+}
+
+(async()=> {
+    main()
+})()
